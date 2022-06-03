e9644a9cf0d0797ccde6dd6fe062f1e47ace2a1f
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2022-06-03 02:03:56 +0200

    Working

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,3 @@
-#!/bin/sh
-
-# Run commands
-cmd=$@
-echo "Running '$cmd'!"
-if $cmd; then
-    # no op
-    echo "Successfully ran '$cmd'"
-else
-    exit_code=$?
-    echo "Failure running '$cmd', exited with $exit_code"
-    exit $exit_code
-fi
+#!/bin/ash
+set -e
+sh -c $@
